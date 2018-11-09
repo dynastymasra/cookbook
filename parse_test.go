a891804f456f60580dc02c6e36772ca5bcc34ee2
Dimas Ragil T <dynastymasra@gmail.com>
2018-11-09 17:16:48 +0700

    Add test for parse

---
+++ parse_test.go
@@ -0,0 +1,62 @@
+package cookbook_test
+
+import (
+	"testing"
+
+	"github.com/dynastymasra/cookbook"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
+)
+
+type ParseSuite struct {
+	suite.Suite
+}
+
+func Test_ParseSuit(t *testing.T) {
+	suite.Run(t, new(ParseSuite))
+}
+
+func (p *ParseSuite) Test_ParseStringToInt_Success() {
+	result := cookbook.ParseStringToInt("10", 0)
+
+	assert.NotZero(p.T(), result)
+	assert.Equal(p.T(), 10, result)
+}
+
+func (p *ParseSuite) Test_ParseStringToInt_Failed_ReturnDefaultValue() {
+	result := cookbook.ParseStringToInt("", 10)
+
+	assert.NotZero(p.T(), result)
+	assert.Equal(p.T(), 10, result)
+}
+
+func (p *ParseSuite) Test_ParseStringToFloat64_Success() {
+	result := cookbook.ParseStringToFloat64("5.25", 10.5)
+
+	assert.NotZero(p.T(), result)
+	assert.Equal(p.T(), 5.25, result)
+}
+
+func (p *ParseSuite) Test_ParseStringToFloat64_Failed_ReturnDefaultValue() {
+	result := cookbook.ParseStringToFloat64("", 10.5)
+
+	assert.NotZero(p.T(), result)
+	assert.Equal(p.T(), 10.5, result)
+}
+
+func (p *ParseSuite) Test_Stringify_Success() {
+	str := "Test parse interface"
+
+	result := cookbook.Stringify(str)
+
+	assert.NotEmpty(p.T(), result)
+}
+
+func (p *ParseSuite) Test_Stringify_Failed_Marshal() {
+	ch := make(chan string)
+
+	result := cookbook.Stringify(ch)
+
+	assert.NotEmpty(p.T(), result)
+}
