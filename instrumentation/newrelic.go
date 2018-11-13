e9f10a73194333ce420b810f49c175417a8e012b
Dimas Ragil T <dynastymasra@gmail.com>
2018-11-13 13:42:31 +0700

    Add new relic handler

--- instrumentation/newrelic.go
+++ instrumentation/newrelic.go
@@ -1,6 +1,11 @@
 package instrumentation
 
-import "github.com/newrelic/go-agent"
+import (
+	"context"
+	"net/http"
+
+	"github.com/newrelic/go-agent"
+)
 
 // NewRelicApplication create new relic application
 func NewRelicApplication(config newrelic.Config) (newrelic.Application, error) {
@@ -9,4 +14,41 @@ func NewRelicApplication(config newrelic.Config) (newrelic.Application, error) {
 		return nil, err
 	}
 	return app, nil
+}
+
+// NewRelicDataStoreSegment used for send database data to new relic
+// defer NewRelicDataStoreSegment(...)()
+func NewRelicDataStoreSegment(ctx context.Context, product newrelic.DatastoreProduct, queryParams map[string]interface{},
+	collection, operation, query, host, port, databaseName string) func() {
+	if trx := newrelic.FromContext(ctx); trx != nil {
+		segment := &newrelic.DatastoreSegment{
+			StartTime:          newrelic.StartSegmentNow(trx),
+			Product:            product,
+			QueryParameters:    queryParams,
+			Collection:         collection,
+			Operation:          operation,
+			ParameterizedQuery: query,
+			Host:               host,
+			PortPathOrID:       port,
+			DatabaseName:       databaseName,
+		}
+		return func() { segment.End() }
+	}
+	return func() {}
+}
+
+// NewRelicExternalSegment used for send external data request to new relic
+// segment := NewRelicExternalSegment(...)
+// resp, err := client.Do(request)
+// segment.Response = resp
+// segment.End()
+func NewRelicExternalSegment(ctx context.Context, request *http.Request, url string) *newrelic.ExternalSegment {
+	if trx := newrelic.FromContext(ctx); trx != nil {
+		return &newrelic.ExternalSegment{
+			StartTime: newrelic.StartSegmentNow(trx),
+			URL:       url,
+			Request:   request,
+		}
+	}
+	return &newrelic.ExternalSegment{}
 }
--- negroni/middleware/newrelic.go
+++ negroni/middleware/newrelic.go
@@ -1,17 +1,19 @@
 package middleware
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/dynastymasra/cookbook"
+	"github.com/gorilla/mux"
 	"github.com/newrelic/go-agent"
 	"github.com/urfave/negroni"
 )
 
 // NewRelicHandler use new relic handler in middleware
-func NewRelicHandler(app newrelic.Application, pattern string) negroni.HandlerFunc {
+func NewRelicHandler(app newrelic.Application) negroni.HandlerFunc {
 	return negroni.HandlerFunc(func(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
-		_, nextHandler := newrelic.WrapHandleFunc(app, pattern, next)
+		_, nextHandler := newrelic.WrapHandleFunc(app, fmt.Sprintf("%s - %s", r.Method, getRoutePath(r)), next)
 		nextHandler(w, r)
 	})
 }
@@ -25,4 +27,13 @@ func NewRelicInstrumentation() negroni.HandlerFunc {
 			next(w, newrelic.RequestWithTransactionContext(r, trx))
 		}
 	})
+}
+
+func getRoutePath(r *http.Request) string {
+	if route := mux.CurrentRoute(r); route != nil {
+		if path, err := route.GetPathTemplate(); err != nil {
+			return path
+		}
+	}
+	return r.URL.Path
 }
