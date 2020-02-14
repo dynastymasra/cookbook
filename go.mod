1996f3ebaac6daf3668670c09aabdfac9335cbb6
Dimas Ragil T <dynastymasra@gmail.com>
2020-02-14 11:33:35 +0700

    Change Auth handler and add logrus

--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/newrelic/go-agent v3.3.0+incompatible
 	github.com/opentracing/opentracing-go v1.1.0 // indirect
 	github.com/satori/go.uuid v1.2.0
+	github.com/sirupsen/logrus v1.2.0
 	github.com/spf13/viper v1.6.2
 	github.com/stretchr/testify v1.4.0
 	github.com/urfave/negroni v1.0.0
---
+++ grpc/interceptor.go
@@ -0,0 +1,88 @@
+package grpc
+
+import (
+	"context"
+	"time"
+
+	"github.com/sirupsen/logrus"
+	"google.golang.org/grpc"
+
+	"github.com/dynastymasra/cookbook"
+
+	grpc_auth "github.com/grpc-ecosystem/go-grpc-middleware/auth"
+
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+/**
+AuthUnaryInterceptor Function for check authentication send by client
+Example: Bearer token
+Ref: https://github.com/grpc-ecosystem/go-grpc-middleware
+*/
+func AuthUnaryInterceptor(expectedToken string) grpc_auth.AuthFunc {
+	return func(ctx context.Context) (context.Context, error) {
+		token, err := grpc_auth.AuthFromMD(ctx, cookbook.Bearer)
+		if err != nil {
+			return nil, err
+		}
+
+		if token != expectedToken {
+			return nil, status.Errorf(codes.Unauthenticated, "invalid token")
+		}
+
+		return ctx, nil
+	}
+}
+
+/**
+LogrusUnaryInterceptor gRPC interceptor to log unary request duration status
+*/
+func LogrusUnaryInterceptor(logger *logrus.Entry) grpc.UnaryServerInterceptor {
+	return func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
+		startTime := time.Now()
+
+		resp, err := handler(ctx, req)
+
+		log := logger.WithFields(logrus.Fields{
+			"full_method":  info.FullMethod,
+			"request_time": startTime.Format(time.RFC3339),
+		})
+
+		responseTime := time.Now()
+		deltaTime := responseTime.Sub(startTime)
+
+		log.WithFields(logrus.Fields{
+			"response_time": responseTime.Format(time.RFC3339),
+			"delta_time":    deltaTime,
+		}).Infoln("gRPC request")
+
+		return resp, err
+	}
+}
+
+/**
+LogrusStreamInterceptor gRPC interceptor to log stream request duration status
+*/
+func LogrusStreamInterceptor(logger *logrus.Entry) grpc.StreamServerInterceptor {
+	return func(srv interface{}, stream grpc.ServerStream, info *grpc.StreamServerInfo, handler grpc.StreamHandler) error {
+		startTime := time.Now()
+
+		err := handler(srv, stream)
+
+		log := logger.WithFields(logrus.Fields{
+			"full_method":  info.FullMethod,
+			"request_time": startTime.Format(time.RFC3339),
+		})
+
+		responseTime := time.Now()
+		deltaTime := responseTime.Sub(startTime)
+
+		log.WithFields(logrus.Fields{
+			"response_time": responseTime.Format(time.RFC3339),
+			"delta_time":    deltaTime,
+		}).Infoln("gRPC request")
+
+		return err
+	}
+}
