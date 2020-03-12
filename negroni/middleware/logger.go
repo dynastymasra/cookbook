8df16e22c368541f9977cae3c2630d5d05e55a6d
Dimas Ragil T <dynastymasra@gmail.com>
2020-03-12 12:38:42 +0700

    Add request_id in log

--- negroni/middleware/logger.go
+++ negroni/middleware/logger.go
@@ -4,6 +4,8 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/dynastymasra/cookbook"
+
 	"github.com/sirupsen/logrus"
 
 	"github.com/urfave/negroni"
@@ -12,6 +14,7 @@ import (
 func LogrusLog(name string) negroni.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
 		startTime := time.Now().UTC()
+		requestID := r.Header.Get(cookbook.XRequestID)
 
 		next(w, r)
 
@@ -19,14 +22,15 @@ func LogrusLog(name string) negroni.HandlerFunc {
 		deltaTime := responseTime.Sub(startTime)
 
 		logrus.WithFields(logrus.Fields{
-			"request_time":   startTime.Format(time.RFC3339),
-			"delta_time":     deltaTime,
-			"response_time":  responseTime.Format(time.RFC3339),
-			"request_proxy":  r.RemoteAddr,
-			"url":            r.URL.Path,
-			"method":         r.Method,
-			"request_source": r.Header.Get("X-FORWARDED-FOR"),
-			"headers":        r.Header,
+			"request_time":     startTime.Format(time.RFC3339),
+			"delta_time":       deltaTime,
+			"response_time":    responseTime.Format(time.RFC3339),
+			"request_proxy":    r.RemoteAddr,
+			"url":              r.URL.Path,
+			"method":           r.Method,
+			"request_source":   r.Header.Get("X-FORWARDED-FOR"),
+			"headers":          r.Header,
+			cookbook.RequestID: requestID,
 		}).Infoln("HTTP Request", name)
 	}
 }
