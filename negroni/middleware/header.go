2a8882606bc6ded85ca980a52ebde4ce5f502a63
Dimas Ragil Triatmaja <dynastymasra@gmail.com>
2020-05-06 11:04:24 +0700

    Change random alpanumeric unstead uuid

--- negroni/middleware/header.go
+++ negroni/middleware/header.go
@@ -4,8 +4,9 @@ import (
 	"context"
 	"net/http"
 
+	"github.com/labstack/gommon/random"
+
 	"github.com/dynastymasra/cookbook"
-	"github.com/satori/go.uuid"
 	"github.com/urfave/negroni"
 )
 
@@ -14,7 +15,7 @@ func RequestID() negroni.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
 		requestID := r.Header.Get(cookbook.XRequestID)
 		if len(requestID) < 1 {
-			requestID = uuid.NewV4().String()
+			requestID = random.New().String(12, random.Alphanumeric)
 		}
 		next(w, r.WithContext(context.WithValue(r.Context(), cookbook.RequestID, requestID)))
 	}
