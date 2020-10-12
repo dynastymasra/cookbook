7c2f7f0afcd7fc4fc68ac32ffaa67d0271da46e7
Dimas Ragil Triatmaja <dynastymasra@gmail.com>
2020-10-12 15:54:52 +0700

    Add function filter in mongo

--- provider/mongo/query.go
+++ provider/mongo/query.go
@@ -1,6 +1,11 @@
 package mongo
 
-import "go.mongodb.org/mongo-driver/mongo/options"
+import (
+	"fmt"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
 
 type Query struct {
 	Limit     int
@@ -21,7 +26,11 @@ type Ordering struct {
 }
 
 const (
-	Equal = "Equal"
+	Equal            = "Equal"
+	LessThan         = "LessThan"
+	LessThanEqual    = "LessThanEqual"
+	GreaterThan      = "GreaterThan"
+	GreaterThanEqual = "GreaterThanEqual"
 
 	Descending = "Descending"
 	Ascending  = "Ascending"
@@ -54,6 +63,22 @@ func NewOrdering(field, direction string) *Ordering {
 	}
 }
 
+// NewFilter creates a new property filter
+func NewFilter(field, condition string, value interface{}) *Filter {
+	return &Filter{
+		Field:     field,
+		Condition: condition,
+		Value:     value,
+	}
+}
+
+// Filter adds a filter to the query
+func (q *Query) Filter(property, condition string, value interface{}) *Query {
+	filter := NewFilter(property, condition, value)
+	q.Filters = append(q.Filters, filter)
+	return q
+}
+
 func (q *Query) Slice(offset, limit int) *Query {
 	q.Offset = offset
 	q.Limit = limit
@@ -71,6 +96,43 @@ func (q *Query) Ordering(property, direction string) *Query {
 func TranslateQuery(query *Query) (interface{}, *options.FindOptions) {
 	limit := int64(query.Limit)
 	skip := int64(query.Offset)
+
+	d := bson.D{}
+
+	for _, filter := range query.Filters {
+		switch filter.Condition {
+		case Equal:
+			d = append(d, bson.E{
+				Key:   fmt.Sprintf("%v", filter.Field),
+				Value: filter.Value,
+			})
+		case GreaterThan:
+			d = append(d, bson.E{
+				Key:   fmt.Sprintf("%v", filter.Field),
+				Value: bson.M{"$gt": filter.Value},
+			})
+		case GreaterThanEqual:
+			d = append(d, bson.E{
+				Key:   fmt.Sprintf("%v", filter.Field),
+				Value: bson.M{"$gte": filter.Value},
+			})
+		case LessThan:
+			d = append(d, bson.E{
+				Key:   fmt.Sprintf("%v", filter.Field),
+				Value: bson.M{"$lt": filter.Value},
+			})
+		case LessThanEqual:
+			d = append(d, bson.E{
+				Key:   fmt.Sprintf("%v", filter.Field),
+				Value: bson.M{"$lte": filter.Value},
+			})
+		default:
+			d = append(d, bson.E{
+				Key:   fmt.Sprintf("%v", filter.Field),
+				Value: filter.Value,
+			})
+		}
+	}
 
 	for _, order := range query.Orderings {
 		switch order.Direction {
