2865a65686f1307b52624a74ebc8c232f06f8bd2
Dimas Ragil Triatmaja <dynastymasra@gmail.com>
2020-10-13 17:39:47 +0700

    Update check error from postgres

--- provider/postgres/error.go
+++ provider/postgres/error.go
@@ -1,19 +1,19 @@
 package postgres
 
 import (
-	"github.com/lib/pq"
+	"github.com/jackc/pgconn"
 	"gorm.io/gorm"
 )
 
 func IsDuplicate(err error) bool {
-	if err, ok := err.(*pq.Error); ok && err.Code == "23505" {
+	if err, ok := err.(*pgconn.PgError); ok && err.Code == "23505" {
 		return true
 	}
 	return false
 }
 
 func IsForeignNotFound(err error) bool {
-	if err, ok := err.(*pq.Error); ok && err.Code == "23503" {
+	if err, ok := err.(*pgconn.PgError); ok && err.Code == "23503" {
 		return true
 	}
 	return false
